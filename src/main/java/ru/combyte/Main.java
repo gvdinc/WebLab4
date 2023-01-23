22ae0cd4754ccaadc146c5a5024e0531ebf32f4f
suuuuuuu <335056@niuitmo.ru>
2023-01-23 06:34:14 +0300

    security

--- src/main/java/ru/combyte/Main.java
+++ src/main/java/ru/combyte/Main.java
@@ -4,21 +4,65 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @PropertySource("classpath:application.properties")
+@EnableWebSecurity(debug = true)
 @SpringBootApplication
-public class Main {
+public class Main implements WebMvcConfigurer {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/login", "/logout", "/register", "/shot", "/shots").hasRole("USER");
         http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
+                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                .and()
+                .cors()
+                .and()
+                .csrf().disable();
         return http.build();
+    }
+//        @Bean
+//        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//            return http.authorizeRequests().requestMatchers("/shot", "/shots")
+//                    .hasAnyRole("USER")
+//                    .requestMatchers("/login", "/logout", "register")
+//                    .permitAll()
+//                    .and()
+//                    .cors()
+//                    .and()
+//                    .csrf().disable()
+//                    .logout().logoutUrl("/logout").logoutSuccessUrl("/login").and().httpBasic().and().build();
+//        }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
